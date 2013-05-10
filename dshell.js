062b1df300de0a94fda5b2646c8ec14da15383fe
apphouse-sap <dominik.tornow@sap.com>
2013-05-10 17:18:18 -0600

    Update dshell.js

--- dshell.js
+++ dshell.js
@@ -1,28 +1,20 @@
 repMiddleware = function (req, res, next) {
 	try {
-
-		res.write("Deployment Shell v1.0.0\n");
-		res.write("Copyright 2013 SAP Labs, LLC\n");
-		res.write("\n");
-		res.write("- Dominik Tornow <dominik.tornow@sap.com>\n");
-		res.write("- Joerg Latza <joerg.latza@sap.com>\n");
-		res.write("\n");
-
 		var rep = require('url').parse(req.url, true).query.repo;
-
 		if(rep) {
 			req.rep = rep;
 			next();
 		}
 		else {
+			res.write("Deployment Shell v1.0.0\n");
+			res.write("Copyright 2013 SAP Labs, LLC\n");
+			res.write("\n");
+			res.write("- Dominik Tornow <dominik.tornow@sap.com>\n");
+			res.write("- Joerg Latza <joerg.latza@sap.com>\n");
 			res.end();
 		}
 	} catch (e) {
-		res.write("ERROR\n");
-		res.write("=====\n");
-		res.write("\n");
-		res.write(e.toString());
-		res.end();
+		res.end(e.toString());
 	}
 }
 
@@ -59,19 +51,17 @@ var app = require('connect')()
 	.use(function(req, res){
 		require('child_process').exec("git clone " + req.rep + " repo", {cwd: req.dir}, function(err, stdout, stderr) {
 
-			console.log(global.username, global.password)
-			console.log(err, stderr)
-
 			res.write("Deployment Shell v1.0.0\n");
 			res.write("Copyright 2013 SAP Labs, LLC\n");
+			res.write("\n");
+			res.write("- Dominik Tornow <dominik.tornow@sap.com>\n");
+			res.write("- Joerg Latza <joerg.latza@sap.com>\n");
 			res.write("\n");
 
 			if(err || stderr) {
 				res.write("ERROR\n");
 				res.write("=====\n");
-				res.write("\n");
 				res.write("Cannot git clone " + req.rep + "\n");
-				res.write("\n");
 				res.write(err.toString());
 				res.write(stderr.toString());
 				res.end();
