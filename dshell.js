09d5c6962405c2f5cc786410ca870d4fa48ce9c2
Gregor Wolf <gregor.wolf@gmail.com>
2016-07-31 19:08:04 +0100

    Remove basicAuth which is not part of connect anymore, adjust error handling

--- dshell.js
+++ dshell.js
@@ -55,81 +55,86 @@ dirMiddleware = function (req, res, next) {
 }
 
 var app = require('connect')()
-    // .use(require('connect').static('strategies'))
-    .use(require('connect').basicAuth(function(username, password) {
-    	// that's right, sue me >:) !!!
-    	global.username = username.toLowerCase(); 
-    	global.password = password; 
-    	return true
-    }))
 	.use(repMiddleware)
 	.use(dirMiddleware)
 	.use(function(req, res) {
 		
 		console.log("git clone " + req.rep);
 		
-		require('child_process').exec("git clone " + req.rep + " repo", {cwd: req.dir, timeout: 25*60*1000}, function(err, stdout, stderr) {
+		require('child_process')
+            .exec("git clone " 
+              + req.rep + " repo", 
+              {cwd: req.dir, timeout: 25*60*1000}, 
+              function(err, stdout, stderr) {
 
-			res.write("Deployment Shell v1.0.0\n");
-			res.write("Copyright 2013 SAP Labs, LLC\n");
-			res.write("\n");
-			res.write("- Dominik Tornow <dominik.tornow@sap.com>\n");
-			res.write("- Joerg Latza <joerg.latza@sap.com>\n");
-			res.write("\n");
+                res.write("Deployment Shell v1.0.0\n");
+                res.write("Copyright 2013 SAP Labs, LLC\n");
+                res.write("\n");
+                res.write("- Dominik Tornow <dominik.tornow@sap.com>\n");
+                res.write("- Joerg Latza <joerg.latza@sap.com>\n");
+                res.write("\n");
 
-			if(err || stderr) {
-				res.write("ERROR\n");
-				res.write("=====\n");
-				res.write("Cannot git clone " + req.rep + "\n");
-				res.write(err.toString());
-				res.write(stderr.toString());
-				res.end();
-				return;
-			}
+                if(err) {
+                    res.write("ERROR\n");
+                    res.write("=====\n");
+                    res.write("Cannot git clone " + req.rep + "\n");
+                    res.write('err: ' + err.toString());
+                    console.log('err: ' + err.toString());
+                    if(stderr) {
+                        res.write('stderr: ' + stderr.toString());
+                        console.log('stderr: ' + stderr.toString());
+                    }
+                    if(stdout) {
+                        res.write('stdout: ' + stdout.toString());
+                        console.log('stdout: ' + stdout.toString());
+                    }
+                    res.end();
+                    return;
+                }
 
-			var deploy = {};
+                var deploy = {};
 
-			try {
-				deploy = JSON.parse(require('fs').readFileSync(req.dir + "/repo/deployment.json"));
-			} catch(ex) {
-				res.write("ERROR\n");
-				res.write("=====\n");
-				res.write("Cannot access or decode deploment descriptor\n");
-				res.write(ex.toString());
-				res.end();
-				return;
-			}
+                try {
+                    deploy = JSON.parse(require('fs').readFileSync(req.dir + "/repo/deployment.json"));
+                } catch(ex) {
+                    res.write("ERROR\n");
+                    res.write("=====\n");
+                    res.write("Cannot access or decode deploment descriptor\n");
+                    res.write(ex.toString());
+                    res.end();
+                    return;
+                }
 
-			res.write("DEPLOYMENT DESCRIPTOR\n");
-			res.write("=====================\n");
-			res.write("\n");
-			res.write(JSON.stringify(deploy));
-			res.write("\n");
+                res.write("DEPLOYMENT DESCRIPTOR\n");
+                res.write("=====================\n");
+                res.write("\n");
+                res.write(JSON.stringify(deploy));
+                res.write("\n");
 
-			try {
-				require(deploy.strategy)(req.dir + "/repo", deploy, function(err, msg) {
-					if(err) {
-						res.write("ERROR\n");
-						res.write("=====\n");
-						res.write("Deployment failed\n");
-						res.write(err.toString());
-						res.end();
-						return;
-					}
-					res.write("SUCCESS\n");
-					res.write("=======\n");
-					res.write("\n");
-					res.end(msg);
-				});
-			} catch(ex) {
-				res.write("ERROR\n");
-				res.write("=====\n");
-				res.write("\n");
-				res.write("Cannot require or execute " + deploy.strategy + "\n");
-				res.write("\n");
-				res.write(ex.toString());
-				res.end();
-				return;
-			}
-		})
+                try {
+                    require(deploy.strategy)(req.dir + "/repo", deploy, function(err, msg) {
+                        if(err) {
+                            res.write("ERROR\n");
+                            res.write("=====\n");
+                            res.write("Deployment failed\n");
+                            res.write(err.toString());
+                            res.end();
+                            return;
+                        }
+                        res.write("SUCCESS\n");
+                        res.write("=======\n");
+                        res.write("\n");
+                        res.end(msg);
+                    });
+                } catch(ex) {
+                    res.write("ERROR\n");
+                    res.write("=====\n");
+                    res.write("\n");
+                    res.write("Cannot require or execute " + deploy.strategy + "\n");
+                    res.write("\n");
+                    res.write(ex.toString());
+                    res.end();
+                    return;
+                }
+            })
 	}).listen(1237);
